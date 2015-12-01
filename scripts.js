8f34912fa47654fc2fb4c6ee867a9351940346ad
wranvaud <wiranvaud@gmail.com>
2015-12-01 12:20:22 -0200

    Organizing code, separating in different files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+window.onload= function() {
+  var autoReloadCheckbox = document.getElementById("checkreload");;
+  autoReloadCheckbox.onchange=autoReload;
+  function autoReload (checked) {
+    console.log(this.getElementsByTagName('input').value);
+    setTimeout(function(){
+      //window.location.reload(1);
+    }, 1000);
+
+  }
+}
